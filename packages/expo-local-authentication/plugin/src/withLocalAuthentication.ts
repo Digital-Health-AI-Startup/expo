26ab615765057dadf54f6092ba78a7954eed6ad8
Eddie Canales <eddiecanales@gmail.com>
2023-12-13 22:18:45 -0500

    change package name for easier publishing

--- packages/expo-local-authentication/plugin/src/withLocalAuthentication.ts
+++ packages/expo-local-authentication/plugin/src/withLocalAuthentication.ts
@@ -1,6 +1,6 @@
 import { AndroidConfig, ConfigPlugin, createRunOncePlugin } from 'expo/config-plugins';
 
-const pkg = require('expo-local-authentication/package.json');
+const pkg = require('expo-local-authentication-with-pin/package.json');
 const FACE_ID_USAGE = 'Allow $(PRODUCT_NAME) to use Face ID';
 
 const withLocalAuthentication: ConfigPlugin<{ faceIDPermission?: string } | void> = (
